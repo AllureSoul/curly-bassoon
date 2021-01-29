903ef615a5a2c549e3f838897ed6ea5bdf322deb
AllureSoul <61911991+AllureSoul@users.noreply.github.com>
2021-01-29 09:48:43 +0700

    Added basic functionality

--- bot.js
+++ bot.js
@@ -7,7 +7,15 @@ app.get('/', (req, res) => res.send('Ropes is currently Online'));
 app.listen(port, () => console.log(`Ropes listening at http://localhost:${port}`));
 
 
-// -- Bot --
-const Discord = require(discord.js);
-const { Client, MessageEmbed } = require(discord.js);
+// -- Bot Login --
+const Discord = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const client = new Discord.Client
+client.login(process.env.DISCORD_TOKEN)
+
+// -- Bot --
+client.on('ready', () => {
+	console.log(`I'm ready!`);
+	client.guilds.cache.get('791720839275741194').channels.cache.get('791723808980598784').send('I Am Online!')
+	client.user.setActivity("Your Game (^w^)/", {type: 'WATCHING' });
+	
